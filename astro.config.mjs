dd0f3f3b483508396c45e2da70f92e9f669410c5
松井貴史 <39471524+TAKUOO@users.noreply.github.com>
2026-04-02 22:27:42 +0900

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL ?? "https://goodcoiner.com",
   vite: {
     plugins: [tailwindcss()],
   },
